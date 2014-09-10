766ea2af03a65c2caa87e44e73fac1fac94061c0
Simon Clifford <simon.j.clifford@gmail.com>
2014-09-10 20:20:11 +0100

    First pass at moving all config into one or two files.

--- html/orcid-auth.php
+++ html/orcid-auth.php
@@ -10,7 +10,6 @@ include_once '../uportal/oauth/OAuthRequester.php';
 require '../uportal/smarty/libs/Smarty.class.php';
 require '../uportal/uportal-config/uportal-functions2.inc';
 require '../uportal/uportal-config/dspace-functions.inc';
-require '../uportal/uportal-config/orcidconfig.inc';
 
 session_start();
 
@@ -28,15 +27,15 @@ print_r( $_SESSION );
 if( !array_key_exists( 'code', $_GET ) ) {
     $_SESSION["oauthnonce"] = md5(uniqid(rand(), true));
     $redirect_uri = "$proto://$host$uri/orcid-auth.php?state=" . $_SESSION["oauthnonce"];
-    header( 'Location: https://orcid.org/oauth/authorize?client_id=' . $orcid_client_id .'&response_type=code&scope=/authenticate&redirect_uri=' . $redirect_uri );
+    header( 'Location: https://orcid.org/oauth/authorize?client_id=' . $UP_config['orcid_client_id'] .'&response_type=code&scope=/authenticate&redirect_uri=' . $redirect_uri );
 }
 else {
 //  curl -i -L -H 'Accept: application/json' --data 'client_id=0000-0001-7197-7095&client_secret=2801423d-88b0-4809-b6d8-87eede5ec00c&grant_type=authorization_code&code=MsjXNS' 'https://api.sandbox.orcid.org/oauth/token'
     $req = new HttpRequest( "https://pub.orcid.org/oauth/token", HTTP_METH_POST );
     $req->addHeaders( array("Accept"=> "application/json")  );
     $req->addPostFields ( array(
-            "client_id" => $orcid_client_id,
-            "client_secret" => $orcid_client_secret,
+            "client_id" => $UP_config['orcid_client_id'],
+            "client_secret" => $UP_config['orcid_client_secret'],
             "grant_type" => "authorization_code",
             "code" => $_GET["code"]
         )  );
