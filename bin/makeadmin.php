98425afcd6e820a6a1cd983b2ed213e0dcdcb5fc
Simon Clifford <simon.j.clifford@gmail.com>
2014-12-03 12:16:26 +0000

    Added script to set/unset users as admins.

---
+++ bin/makeadmin.php
@@ -0,0 +1,42 @@
+#!/usr/bin/php
+<?
+#
+# Changes a uportal user's admin status.
+#
+# Usage: makeadmin.php [-u] username
+#
+
+require_once "../uportal/uportal-config/configuration.inc";
+require_once "../uportal/uportal-config/db-functions.inc";
+
+$remove = false;
+if( count($argv) < 2 or count($argv) > 3 ) { usage();}
+if( $argv[1] == '-u' ) {
+    $remove = true;
+    array_shift( $argv );
+}
+
+if( count($argv) != 2 ) { usage();}
+$user = strtolower($argv[1]);
+print ($remove ? "Unsetting" : "Setting") . " " . $user . "...";
+
+# CBA faffing with PHP -> SQL booleans
+if( $remove ) {
+    $query = "UPDATE users SET is_admin=false WHERE username = ?";
+} else {
+    $query = "UPDATE users SET is_admin=true WHERE username = ?";
+}
+$arr = db_query( $query, array($user) );
+
+if ($arr != 0) {
+    print " success.\n";
+} else {
+    print " failed!\n";
+}
+
+function usage() {
+    print "Usage: makeadmin.php [-u] username\n";
+    print "Sets (-u unsets) username as admin\n";
+    die;
+}
+?>
